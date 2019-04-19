1c698be9b64c91a37d3eb05fd3308d9b24186e05
Justin Ho <justinho515@gmail.com>
2019-04-19 15:30:30 -0700

    moved js to js directory

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
-function addItem() {
+function addItem(){
     var ul = document.getElementById("dynamic-list");
     var candidate = document.getElementById("candidate");
-    var li = document.createElement("li");
-    li.setAttribute("id", candidate.value);
-    li.appendChild(document.createTextNode(candidatevalue));
-    ul.appendChild(li);
+
+    // Don't add item if input field is blank
+    if (candidate.value != "") {
+        var li = document.createElement("li");
+        li.setAttribute('id',candidate.value);
+        li.appendChild(document.createTextNode(candidate.value));
+        ul.appendChild(li);
+    }
+
+    // Clear input field after adding item
+    candidate.value = "";
 }
