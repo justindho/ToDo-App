840aaae54180fb2a196b05d3f86268f2222661d9
Justin Ho <justinho515@gmail.com>
2019-05-26 20:46:53 -0700

    Add ability to add, complete, and delete tasks and to remember the task list using local storage. Add styling to task items.

--- js/script.js
+++ js/script.js
@@ -1,73 +1,60 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    // Create a tasklist in local storage if not already created.
-    // if (!localStorage.getItem('tasklist')) {
-    //     localStorage.setItem('tasklist', () => {
-    //         let ul = document.createElement('ul');
-    //     });
-    // }
-
     // Variables for HTML elements
     let tasklist = document.querySelector('#tasklist');
     let form = document.querySelector('form');
     let task = document.querySelector('#task');
-
-    // By default, the 'add' button for a new task is disabled.
-    document.querySelector('#addbutton').disabled = true;
-
-    // Enable button only if the input field is not empty.
-    document.querySelector('#inputfield').onkeyup = () => {
-        if (document.querySelector('#inputfield').value.length > 0) {
-            document.querySelector('#addbutton').disabled = false;
-        } else {
-            document.querySelector('#addbutton').disabled = true;
-        }
-    }
-
-    // Listen for 'add task' button clicks.
+    
+    /**
+     * When the 'add task' button is clicked, add the new task to 
+     * local storage.
+     */
     form.addEventListener('submit', (event) => {
-
         // Prevent form submissions.
         event.preventDefault();
         
         // Store new task in storage and reset 'new task' input field.
-        tasklist.innerHTML = '<li>' + task.value + '</li>';
+        tasklist.innerHTML += '<li>' + task.value + '</li>';
         store();
         task.value = '';
     });
 
-    // 
-    tasklist.addEventListener('click', (event) => {
-        // Get task that was clicked on. If it's marked as complete, 
-        // save that info to local storage.
+    /**
+     * Get task that was clicked on. If it was already marked as complete,
+     * remove the task. Else, add a check to the task to mark it as complete.
+     * Update local storage.
+     */
+    tasklist.addEventListener('click', (event) => {           
         let t = event.target;
         if (t.classList.contains('checked')) {
             t.parentNode.removeChild(t);
+        } else {
+            t.classList.add('checked');
         }
+        store();
     });
 
-    function store() {
-        window.localStorage.myTasks = tasklist.innerHTML;
+    /**
+     * Update local storage with the updated task list's HTML.
+     */
+    function store() {        
+        localStorage.setItem('myTasks', tasklist.innerHTML);
     };
 
-    // Add new task to tasklist.
-    document.addEventListener('DOMContentLoaded', () => {
-        
-        document.querySelector('#new-task').onsubmit = () => {
+    /**
+     * Update UI with user's task list.
+     */
+    function getValues() {        
+        let storedValues = localStorage.getItem('myTasks');
 
-            // Create a new task for the tasklist.
-            const li = document.createElement('li');
-            li.innerHTML = document.querySelector('#inputfield').value;
-
-            // Add new task to tasklist.
-            document.querySelector('#tasklist').append(li);
-
-            // Clear input field.
-            document.querySelector('#inputfield').value = '';
-
-            // Stop form from resubmitting.
-            // return false;
-        };
-    });
+        // Create tasklist if it doesn't already exist. Else update UI.        
+        if (!storedValues) {
+            tasklist.innerHTML = '';
+        } else {
+            tasklist.innerHTML = storedValues;
+        }
+    };
+    
+    getValues();
 
 });
